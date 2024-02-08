4d3138c5c879e1665c6a443de2a1dc76d875fb43
Roman Sofin <59724407+Firewal7@users.noreply.github.com>
2024-02-08 17:26:04 +0500

    Delete locals.tf

--- locals.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  instance_master           = "master"
-  instance_node1            = "node1" 
-  instance_node2            = "node2"
-  instance_teamsity-server  = "teamsity-server"
-  instance_teamsity-agent   = "teamsity-agent"
-}
